5b34a6fe680628a82989d429f8ad97270f7acda0
Ted Keim <tedkeim@yahoo.com>
2021-05-08 15:57:38 -0400

    changed html layout with different bootstrap template

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,18 @@
-var APIkey = 'e8441a8f1e76def5cd186ddb0adbbe22'
-var weatherApi = 'https://api.openweathermap.org/data/2.5/forecast?q='
-var city = document.getElementById('currentCity')
-var temp = document.getElementById('currentTemp')
-var wind = document.getElementById('currentWind')
-var date = document.getElementById('date')
-var previous = document.getElementById('previousSearch')
-var input = document.getElementById('locations')
+var APIkey = 'e8441a8f1e76def5cd186ddb0adbbe22';
+var weatherApi = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+var cityName = document.getElementById('currentCity');
+var temp = document.getElementById('currentTemp');
+var wind = document.getElementById('currentWind');
+var date = document.getElementById('date');
+var previous = document.getElementById('previousSearch');
+var input = document.getElementById('locations');
+var url =`https://api.openweathermap.org/data/2.5/weather?q=`;
+var units = '&units=imperial';
 
+// function that will get data when the search btn is clicked on 
 $('#searchBtn').on('click',function() {
-    console.log('does this work?')
-})
+    fetch(url+input.value+APIkey+units)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+

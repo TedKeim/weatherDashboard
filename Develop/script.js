4c050493e7ffce03e742cdf91641211c2e51743e
Ted Keim <tedkeim@yahoo.com>
2021-05-08 14:47:31 -0400

    added variables, started on click function for search button

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,12 @@
 var APIkey = 'e8441a8f1e76def5cd186ddb0adbbe22'
+var weatherApi = 'https://api.openweathermap.org/data/2.5/forecast?q='
+var city = document.getElementById('currentCity')
+var temp = document.getElementById('currentTemp')
+var wind = document.getElementById('currentWind')
+var date = document.getElementById('date')
+var previous = document.getElementById('previousSearch')
+var input = document.getElementById('locations')
+
+$('#searchBtn').on('click',function() {
+    console.log('does this work?')
+})
